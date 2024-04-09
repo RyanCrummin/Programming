443cdcb3f9e3df78c87fbadebdbd1e07ca57b0f5
RyanCrumley-STUDENT <RyanCrumley-STUDENT@LAB-B2306-22>
2024-04-09 16:41:06 +0100

    Add project files.

---
+++ Inheritcane Week8/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Inheritcane_Week8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

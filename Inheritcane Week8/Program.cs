5c0198c0d149c6cf031d35cf32531bf871f5642a
Ryan Crumley <S00253911@atu.ie>
2024-04-09 17:46:21 +0100

    Finished Q1

--- Inheritcane Week8/Program.cs
+++ Inheritcane Week8/Program.cs
@@ -1,10 +1,30 @@
 ﻿namespace Inheritcane_Week8
-{
+{/* name: Ryan Crumley
+  * Date: 09/04/2024
+  */
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Employee employ1 = new Employee();
+            Employee employ2 = new Employee("Ryan","Male",19,"Irish");
+
+            Console.WriteLine(employ1.ToString());
+            Console.WriteLine(employ2.ToString());
+
+            Employee employ3 = new Employee();
+            employ3.Name = "john";
+            employ3.Gender = "Male";
+            employ3.HourlyRate = 100;
+            employ3.Nationality = "American";
+            
+          
+            Console.WriteLine(employ3);
+
+            Console.WriteLine(employ3.CalcTax());
+
+            
+
         }
     }
 }
